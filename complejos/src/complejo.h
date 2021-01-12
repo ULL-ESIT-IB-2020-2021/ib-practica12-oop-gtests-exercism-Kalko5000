8f231b67ce97ab5c405dceb846a945040bf83cc7
usuario <thomasattenerife@gmail.com>
2021-01-12 18:42:22 +0000

    basics

---
+++ complejos/src/complejo.h
@@ -0,0 +1,10 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Thomas Edward Bradley
+ * @date 12.jan.2020
+ * @brief Meter texto
+ */
---
+++ complejos/src/complejos.cc
@@ -0,0 +1,22 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Thomas Edward Bradley
+ * @date 12.jan.2020
+ * @brief El programa muestra por pantalla los resultados de varios calculos llevados
+ *        a cabo con numeros complejos
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include "complejo.h"
+using namespace std;
+
+int main (int argc,char* argv[]) {
+    
+}
